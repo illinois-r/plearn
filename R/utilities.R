1b7f986c6ab6e916e3e30a15ea1650f911e948e0
James Balamuta <balamut2@illinois.edu>
2019-08-08 00:25:23 -0500

    Add utilities

---
+++ R/utilities.R
@@ -0,0 +1,23 @@
+has_name = function(name) {
+  if(missing(name) || is.null(name)) stop("Must supply a `name`.")
+}
+
+# Harvested from usethis
+check_file_name <- function(name) {
+  if (!valid_file_name(fs::path_ext_remove(name))) {
+    ui_stop(c(
+      "{ui_value(name)} is not a valid file name. It should:",
+      "* Contain only ASCII letters, numbers, '-', and '_'."
+    ))
+  }
+  name
+}
+
+valid_file_name <- function(x) {
+  grepl("^[a-zA-Z0-9._-]+$", x)
+}
+
+compact <- function(x) {
+  is_empty <- vapply(x, function(x) length(x) == 0, logical(1))
+  x[!is_empty]
+}
