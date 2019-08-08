df7414ab5acc37d459e647f0f51612aa0070ee1d
James Balamuta <balamut2@illinois.edu>
2019-08-08 00:26:27 -0500

    Add an MC-fixed question example.

---
+++ R/multiple-choice.R
@@ -0,0 +1,27 @@
+#' Create a Multiple Choice Question with a Fixed Number of Responses
+#' 
+#' Establishes a multiple choice question with a fixed amount of responses.
+#' 
+#' @template info-json-params
+#' 
+#' @export
+use_mc_fixed = function(name, title = NULL, topic = NULL, tags = NULL) {
+  has_name(name)
+  
+  name <- glue::glue("mc_fixed_{tolower(name)}")
+  check_file_name(name)
+  
+  info_data = list(
+    title = title,
+    topic = topic,
+    tags = make_tags(c("mc", "v3", tags))
+  )
+  
+  use_pl_question_info("info-generic.json", name, data = info_data)
+  use_pl_question_html("question-mc-fixed.html", name)
+}
+
+make_tags = function(tags, author = getOption("plearn.author", "author-unknown")) {
+  unique(c(tags, author))
+}
+
---
+++ man-roxygen/info-json-params.R
@@ -0,0 +1,5 @@
+#' @param name   Instructor-facing name of the question. This will be appended
+#'               with the relevant question type.
+#' @param title  Student-facing name for the question.
+#' @param topic  Category to put the question under.
+#' @param tags   Subtags to identify material tested or technologies used.
