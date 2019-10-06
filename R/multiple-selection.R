113055f3f7f0761891d9f548d1c67c22b0761098
James J Balamuta <balamut2@illinois.edu>
2019-10-05 19:43:44 -0500

    Add a multiple selection template

---
+++ R/multiple-selection.R
@@ -0,0 +1,22 @@
+#' Create a Multiple Choice Question with a Fixed Number of Responses
+#' 
+#' Establishes a multiple choice question with a fixed amount of responses.
+#' 
+#' @template info-json-params
+#' 
+#' @export
+use_mc_fixed = function(name, title = NULL, topic = NULL, tags = NULL) {
+  has_name(name)
+  
+  name <- glue::glue("ms_fixed_{tolower(name)}")
+  check_file_name(name)
+  
+  info_data = list(
+    title = title,
+    topic = topic,
+    tags = make_tags(c("ms", "v3", tags))
+  )
+  
+  use_pl_question_info("info-generic.json", name, data = info_data)
+  use_pl_question_html("question-ms-fixed.html", name)
+}
